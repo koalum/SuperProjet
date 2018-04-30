8bb1bdb4c7655ef38e1457506ab8296dcf2caf5a
kaodi alum <kaodi.alum@insa-lyon.fr>
2018-04-30 16:56:02 +0200

    Classe Metabolite

--- Individu.cpp
+++ Individu.cpp
@@ -1,11 +1,10 @@
 #include"Individu.h"
-#include"Metabolite.h"
 #include <iostream>
 using namespace std;
 
 const float pDeath;
 const float pMut;
 
-//Constructeurs
-//Destructeurs
+//Constructors
+//Destructors
 //Méthodes
---
+++ Metabolite.cpp
@@ -0,0 +1,8 @@
+#include"Metabolite.h"
+#include <iostream>
+using namespace std;
+
+
+//Constructors
+//Destructors
+//Méthodes
