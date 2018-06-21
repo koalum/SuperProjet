f13ba66e8dd652b437421d5eb45e6ad423f3a87f
koalum <33092642+koalum@users.noreply.github.com>
2018-06-21 08:12:26 +0200

    Add files via upload

--- Metabolite.cpp
+++ Metabolite.cpp
@@ -1,27 +1,48 @@
+/******************************************************
+
+                  | INCLUDES |
+
+*******************************************************/
+
 #include"Metabolite.h"
 #include <iostream>
 using namespace std;
 
-//Constructors
-Metabolite::Metabolite(float D, char type, float concentration):D_(D),type_(type), concentration_(concentration){
-}
+/*******************************************************
 
+                 | CONSTRUCTEURS |
+
+********************************************************/
+
+//CONSTRUCTEUR PAR DEFAUT
 Metabolite::Metabolite(){
    D_ = 0.1;
    type_ = 'A';
    concentration_ = 20.0;
 }
 
-//Getters
+//CONSTRUCTEUR PAR COPIE
+Metabolite::Metabolite(float D, char type, float concentration):D_(D),type_(type), concentration_(concentration){
+}
+
+/*******************************************************
+
+                 | GETTERS |
+
+********************************************************/
+
 float Metabolite::concentration(){
    return concentration_;
 }
 
-//Setters
+/*******************************************************
+
+                 | SETTERS |
+
+********************************************************/
+
+//CONCENTRATION
 void Metabolite::concentration(float concentration){
    concentration_=concentration;
 }
 
-
-//Destructors
-//Méthodes
